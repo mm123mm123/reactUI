8e7f4b83aaa08ea6c639588e2a39b7b583238e8c
mm123mm123 <fiymi666@gmail.com>
2021-05-14 17:11:09 +0800

    增加externals之后页面报错
    显示reactDOM为undefined

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const HtmlWebpack = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
         rui: './lib/index.tsx'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
@@ -23,5 +26,19 @@ module.exports = {
             title: 'RUI',
             template: 'index.html'
         })
-    ]
+    ],
+    externals: {
+        react: {
+            commonjs: "react",
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
+    }
 }
