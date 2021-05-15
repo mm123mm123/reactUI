cc630cc02e8e0fb21c537da704637bd6dd7d0c15
mm123mm123 <fiymi666@gmail.com>
2021-05-15 09:00:15 +0800

    开启本地webServer不需要externals否则报错

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpack = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
         rui: './lib/index.tsx'
     },
@@ -27,18 +27,18 @@ module.exports = {
             template: 'index.html'
         })
     ],
-    externals: {
-        react: {
-            commonjs: "react",
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React'
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM'
-        }
-    }
+    // externals: {
+    //     react: {
+    //         commonjs: "react",
+    //         commonjs2: 'react',
+    //         amd: 'react',
+    //         root: 'React'
+    //     },
+    //     'react-dom': {
+    //         commonjs: 'react-dom',
+    //         commonjs2: 'react-dom',
+    //         amd: 'react-dom',
+    //         root: 'ReactDOM'
+    //     }
+    // }
 }
