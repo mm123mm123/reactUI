b703bcb0a878f54dc52f06e2ddacd0583cb651f5
mm123mm123 <fiymi666@gmail.com>
2021-05-18 15:26:31 +0800

    默认导入编译器报错

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import React from 'react';
 
-import * as ReactDom from 'react-dom';
+import ReactDom from 'react-dom';
 import Button from '../lib/Button';
 
 ReactDom.render(<Button/>, document.body);
