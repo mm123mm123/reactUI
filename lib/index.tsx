680494f046e61e7bcff5ab42aa0f1188a457e50d
mimi <fiymi666@gmail.com>
2021-05-23 17:59:17 +0800

    设置svgSpriteLoader，引入svg文件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import ReactDom from 'react-dom';
-import Button from '../lib/Button';
 import Icon from './icon';
+import weichat from './icons/weichat.svg';
 
+console.log(weichat);
 ReactDom.render(<div><Icon name="weChat"/></div>, document.body);
