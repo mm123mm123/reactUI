2225be3ca2aba6f6834ac53dd4b037a835daf982
mimi <fiymi666@gmail.com>
2021-05-23 17:05:43 +0800

    创建icon组件，解决yarn.lock文件依赖冲突

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 
 import ReactDom from 'react-dom';
 import Button from '../lib/Button';
+import Icon from './icon';
 
-ReactDom.render(<Button/>, document.body);
+ReactDom.render(<div><Icon name="weChat"/></div>, document.body);
