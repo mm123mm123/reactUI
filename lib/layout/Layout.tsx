2dde00512bc2b0c98aaaa96f1fdc4d2d41f9071d
mimi <fiymi666@gmail.com>
2021-06-22 09:32:41 +0800

    layout组件添加类名函数

--- lib/layout/Layout.tsx
+++ lib/layout/Layout.tsx
@@ -3,9 +3,15 @@ import React, {HTMLAttributes} from "react";
 interface LayoutProps extends HTMLAttributes<HTMLElement> {
 }
 
+function scopedClassMaker(prefix: string) {
+    return (...suffix: string[]) => [prefix, ...suffix].filter(Boolean).join('-');
+}
+
+const scopedClass = scopedClassMaker('rui-layout');
 const Layout: React.FunctionComponent = (props: LayoutProps) => {
+    const {className, ...restProps} = props
     return (
-        <div>
+        <div className={[scopedClass(), className].filter(Boolean).join(' ')} {...restProps}>
             {props.children}
         </div>
     )
