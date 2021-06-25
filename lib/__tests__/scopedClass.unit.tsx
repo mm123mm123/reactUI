09e535a8cf6b194a614b880184e262bd4ec32f94
mimi <fiymi666@gmail.com>
2021-06-25 19:42:52 +0800

    update

--- lib/__tests__/scopedClass.unit.tsx
+++
@@ -1,9 +0,0 @@
-import scopedClassMaker from "../scopedClass";
-
-const scopedClass = scopedClassMaker('rui-layout');
-
-describe('类名处理函数测试', () => {
-    it('scopedClass(1,2)=3', () => {
-        expect(scopedClass(['1'], '2','3')).toEqual('rui-layout-1 2 3');
-    });
-});
